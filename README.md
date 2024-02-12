20c8b34318128a75cd2e3cea842f306106ad4f86
Veiko Vunder <veix123@gmail.com>
2024-02-12 10:29:48 +0200

    basic bringup system for ROS2 humble: starts the driver and realsense camera

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # robotont\_support
 A package for maintaining bring-up and configuration files
 
-[![Build Status](https://travis-ci.com/robotont/robotont_support.svg?branch=melodic-devel)](https://travis-ci.com/robotont/robotont_support)
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 This package is used as a single entry point for starting background nodes on Robotont's on-board computer. The files are designed to be automatically launched from systemd services located in ```/lib/systemd/system/```. You  may also use the launchers in this package to manually bring up the nodes, however before doing so, make sure that Robotont's systemd services have been stopped.
@@ -16,18 +15,19 @@ roslaunch robotont_support robotont_bringup.launch
 This command will perform the following actions:
 * start the [robotont driver](https://github.com/robotont/robotont_driver)
 * start the [Realsense 3D camera driver](https://github.com/IntelRealSense/realsense-ros)
-* serve the robotont [web application](https://github.com/robotont/robotont_webapp)
-* load the [robot description](https://github.com/robotont/robotont_nuc_description) to the parameter server
-* start the state publishers.
+* ~~serve the robotont [web application](https://github.com/robotont/robotont_webapp)~~
+* ~~load the [robot description](https://github.com/robotont/robotont_nuc_description)~~
+* ~~start the state publishers.~~
 
 
 You can control whether the Realsense camera and the web application are started with the following arguments:
 
 ```bash
-roslaunch robotont_support robotont_bringup.launch realsense:=false webapp:=false
+ros2 launch robotont_support robotont_bringup.launch.py realsense:=false webapp:=false
 ```
 
 ### cam\_throttling.launch
+> :warning: Not yet ported for ROS2
 
 This launch file uses throttling node from ```topic_tools``` package to create additional ```..._throttled``` depth camera topics with slowed down publishing rates. Throttling becomes useful when trying to transfer image/depth streams to a remote computer in limited bandwitdh conditions. Use the ```cam_fps``` argument to set the desired publishing rate for the throttled topics.
 
---
+++ launch/robotont_bringup.launch.py
@@ -0,0 +1,104 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import TextSubstitution 
+
+from launch.conditions import IfCondition
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+  ld = LaunchDescription()
+
+  # Declare the launch arguments
+  realsense_arg = DeclareLaunchArgument(
+    name='realsense',
+    default_value='true',
+    description='Whether to start the realsense camera driver',
+    choices=['true', 'false']
+  )
+
+  webapp_arg = DeclareLaunchArgument(
+    name='webapp',
+    default_value='false',  # Change to true when webapp is ported to ROS2
+    description='Whether to start the webapp',
+    choices=['true', 'false']
+  )
+
+  # Specify the actions
+  robotont_driver_node = Node(
+    package='robotont_driver',
+    executable='driver_basic',
+    name='robotont_driver_node',
+    output='screen'
+  )
+
+  joint_state_publisher_node = Node(
+    package='joint_state_publisher',
+    executable='joint_state_publisher',
+    name='joint_state_publisher_node',
+    output='screen'
+  )
+
+  robot_state_publisher_node = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    name='robot_state_publisher_node',
+    output='screen'
+  )
+
+  should_launch_realsense = LaunchConfiguration('realsense')
+
+  realsense_include = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+      os.path.join(
+        get_package_share_directory('realsense2_camera'),
+        'launch/rs_launch.py'
+      )
+    ),
+    condition=IfCondition(should_launch_realsense)
+  )
+
+  # Set realsense compressedDepth png level to 5
+  realsense_param = {
+    '/camera/aligned_depth_to_color/image_raw/compressedDepth/png_level': 5
+  }
+
+  # Load the robot description
+  upload_description = Node(
+    package='robotont_description',
+    executable='upload_description',
+    name='upload_description',
+    output='screen'
+  )
+
+
+  # Add the actions to the launch description
+  ld.add_action(realsense_arg)
+  # Add realsense camera only if the argument is true
+  ld.add_action(realsense_include)
+
+  #ld.add_action(realsense_param)
+  ld.add_action(webapp_arg)
+  #ld.add_action(robotont_driver_node)
+  #ld.add_action(joint_state_publisher_node)
+  #ld.add_action(robot_state_publisher_node)
+
+  return ld
+
+
+
+# TODO: Add the following to the launch file
+# * robotont_nuc_description/launch/upload_description.launch
+# * robotont_support/launch/cam_throttling.launch
+# * robotont_webapp/launch/webapp.launch
+# * tf_prefix for realsense camera
