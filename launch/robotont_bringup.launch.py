ed7a24326b0a2c3a3d461386a33ce04644638d57
Veiko Vunder <veix123@gmail.com>
2026-01-16 11:39:11 +0200

    made bringup more universal, added namespacing support

--- launch/robotont_bringup.launch.py
+++ launch/robotont_bringup.launch.py
@@ -1,142 +1,138 @@
 import os
-
 from ament_index_python import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch.substitutions import LaunchConfiguration
-from launch.substitutions import TextSubstitution
-
-from launch.conditions import IfCondition
-
 from launch_ros.actions import Node
 
 
+def launch_setup(context, *args, **kwargs):
+    # Get parameter values
+    namespace = LaunchConfiguration('namespace').perform(context)
+    frame_prefix = LaunchConfiguration('frame_prefix').perform(context)
+    generation = LaunchConfiguration('generation').perform(context)
+    realsense_enabled = LaunchConfiguration('realsense').perform(context) == 'true'
+    
+    # If frame_prefix empty, use namespace as prefix
+    if not frame_prefix:
+        frame_prefix = namespace
+
+    print(f"Bringing up Robotont with generation: {generation}, namespace: '{namespace}', frame_prefix: '{frame_prefix}'")            
+    nodes = []
+    
+    # Driver node
+    nodes.append(Node(
+        package='robotont_driver',
+        executable='driver_node',
+        name='driver',
+        namespace=namespace,
+        parameters=[{'frame_prefix': frame_prefix}],
+        output='screen'
+    ))
+    
+    # Generation-specific setup
+    if generation == 'lite3':
+        # Lite3: RPLidar + lite description
+        nodes.append(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('robotont_lite_description'), 'launch/upload_description.launch.py')
+            ),
+            launch_arguments={
+                'namespace': namespace,
+                'frame_prefix': frame_prefix+'/' if frame_prefix else '',
+                'generation': generation,
+                'primary_color': '0.16 0.65 0.98 1.0',
+                'secondary_color': '0.0 1.0 1.0 1.0'
+            }.items()
+        ))
+        
+        # nodes.append(Node(
+        #     package='rplidar_ros',
+        #     executable='rplidar_composition',
+        #     name='rplidar',
+        #     namespace=namespace,
+        #     parameters=[{
+        #         'serial_port': '/dev/ttyUSB0',
+        #         'frame_id': f'{frame_prefix}/laser' if frame_prefix else 'laser',
+        #         'angle_compensate': True
+        #     }],
+        #     output='screen'
+        # ))
+        
+    elif generation in ['2.1', '3'] and realsense_enabled:
+        # Gen 2.1/3: RealSense + depth to laserscan
+        nodes.append(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('robotont_nuc_description'), 'launch/description.launch.py')
+            ),
+            launch_arguments={
+                'namespace': namespace,
+                'frame_prefix': frame_prefix+'/' if frame_prefix else '',
+                'generation': generation
+            }.items()
+        ))
+        
+        nodes.append(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('realsense2_camera'), 'launch/rs_launch.py')
+            ),
+            launch_arguments={
+                'camera_namespace': namespace,
+                'camera_name': 'camera',
+                'align_depth.enable': 'true',
+                'unite_imu_method': '1',
+                'enable_accel': 'true',
+                'enable_gyro': 'true'
+            }.items()
+        ))
+        
+        nodes.append(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('depthimage_to_laserscan'),
+                           'launch/depthimage_to_laserscan-launch.py')
+            )
+        ))
+    else:
+        # Robot without Realsense: use a basic robotont_description
+        nodes.append(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('robotont_description'), 'launch/description.launch.py')
+            ),
+            launch_arguments={
+                'namespace': namespace,
+                'frame_prefix': frame_prefix+'/' if frame_prefix else '',
+                'generation': generation
+            }.items()
+        ))
+
+        
+    nodes.append(Node(
+        package='laserscan_to_ranges',
+        executable='laserscan_to_ranges',
+        name='laserscan_to_ranges',
+        namespace=namespace,
+        output='screen'
+    ))
+    
+    nodes.append(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('demo_teleop'), 'launch/teleop_joy.launch.py')
+        ),
+        launch_arguments={'namespace': namespace}.items()
+    ))
+    
+    return nodes
+
+
 def generate_launch_description():
-  ld = LaunchDescription()
-
-  # Declare the launch arguments
-  realsense_arg = DeclareLaunchArgument(
-    name='realsense',
-    default_value='true',
-    description='Whether to start the realsense camera driver',
-    choices=['true', 'false']
-  )
-
-  webapp_arg = DeclareLaunchArgument(
-    name='webapp',
-    default_value='false',  # Change to true when webapp is ported to ROS2
-    description='Whether to start the webapp',
-    choices=['true', 'false']
-  )
-
-  # Specify the actions
-  robotont_driver_node = Node(
-    package='robotont_driver',
-    executable='driver_node',
-    name='robotont_driver_node',
-    output='screen'
-  )
-
-  joint_state_publisher_node = Node(
-    package='joint_state_publisher',
-    executable='joint_state_publisher',
-    name='joint_state_publisher_node',
-    output='screen'
-  )
-
-  robot_state_publisher_node = Node(
-    package='robot_state_publisher',
-    executable='robot_state_publisher',
-    name='robot_state_publisher_node',
-    output='screen'
-  )
-
-  should_launch_realsense = LaunchConfiguration('realsense')
-
-
-  realsense_include = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(
-      os.path.join(
-        get_package_share_directory('realsense2_camera'),
-        'launch/rs_launch.py'
-      )
-    ),
-    launch_arguments={
-      'camera_namespace': '',
-      'camera_name': 'camera',
-      'align_depth.enable': 'true',
-      'unite_imu_method': '1',
-      'enable_accel': 'True',
-      'enable_gyro': 'True'
-    }.items(),
-    condition=IfCondition(should_launch_realsense)
-  )
-
-  depthimage_to_laserscan_include = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(
-      os.path.join(
-        get_package_share_directory('depthimage_to_laserscan'),
-        'launch/depthimage_to_laserscan-launch.py'
-      )
-    ),
-    condition=IfCondition(should_launch_realsense)
-  )
-
-  laserscan_to_ranges_node = Node(
-    package='laserscan_to_ranges',
-    executable='laserscan_to_ranges',
-    name='laserscan_to_ranges_node',
-    output='screen',
-    condition=IfCondition(should_launch_realsense)
-  )
-
-  # Set realsense compressedDepth png level to 5
-  realsense_param = {
-    '/camera/aligned_depth_to_color/image_raw/compressedDepth/png_level': 5
-  }
-
-  # Teleop Joy Launch
-  teleop_joy_include = IncludeLaunchDescription(
-      PythonLaunchDescriptionSource(
-          os.path.join(
-              get_package_share_directory('demo_teleop'),
-              'launch/teleop_joy.launch.py'
-          )
-      )
-  )
-
-
-  # Load the robot description
-  upload_description = Node(
-    package='robotont_description',
-    executable='upload_description',
-    name='upload_description',
-    output='screen'
-  )
-
-
-  # Add the actions to the launch description
-  ld.add_action(realsense_arg)
-  ld.add_action(webapp_arg)
-  ld.add_action(realsense_include)
-  ld.add_action(robotont_driver_node)
-  ld.add_action(depthimage_to_laserscan_include)
-  ld.add_action(laserscan_to_ranges_node)
-  ld.add_action(teleop_joy_include)
-  #ld.add_action(joint_state_publisher_node)
-  #ld.add_action(robot_state_publisher_node)
-
-  return ld
-
-
-
-# TODO: Add the following to the launch file
-# * robotont_nuc_description/launch/upload_description.launch
-# * robotont_support/launch/cam_throttling.launch
-# * robotont_webapp/launch/webapp.launch
-# * tf_prefix for realsense camera
+    return LaunchDescription([
+        DeclareLaunchArgument('namespace', default_value=''),
+        DeclareLaunchArgument('frame_prefix', default_value=''),
+        DeclareLaunchArgument('generation', default_value='3', choices=['2.1', '3', 'lite3']),
+        DeclareLaunchArgument('realsense', default_value='true', choices=['true', 'false']),
+        
+        # TODO: fake_hardware argument should be added to easily switch between real and fake hardware from a single bringup entrypoint
+        
+        OpaqueFunction(function=launch_setup)
+    ])
